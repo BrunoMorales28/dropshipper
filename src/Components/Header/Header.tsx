a44eefdfefaac379af7058e3aca24f6444f8030f
BrunoMorales28 <128781212+BrunoMorales28@users.noreply.github.com>
2023-04-28 03:16:43 -0300

    Added Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,47 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 const Header = () => {
-  return <div>This is Header</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            textAlign={{ md: "left" }}
+            sx={{ flexGrow: 1 }}
+          >
+            Morales DS.
+          </Typography>
+          <IconButton aria-label="delete">
+            <MenuIcon />
+          </IconButton>
+          <IconButton aria-label="delete">
+            <MenuIcon />
+          </IconButton>
+          <IconButton aria-label="delete">
+            <ShoppingCartIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
